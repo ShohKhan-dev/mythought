695fb51e1843eb566b474d452ff0624db15610be
Shohjahon <shoh.hamraqulov@gmail.com>
2022-03-03 10:41:20 +0300

    ready to deploy

--- core/settings.py
+++ core/settings.py
@@ -14,10 +14,13 @@ from pathlib import Path
 # import sys
 # import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
